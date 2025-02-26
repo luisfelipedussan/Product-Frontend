d7c004add3d2991c90d3248159bb64f10018f3e8
Luis Felipe Dussán Rueda <78167781+luisfelipedussan@users.noreply.github.com>
2025-02-26 17:46:16 -0500

    FRONT END: logo and other fixes

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { toast } from 'react-hot-toast';
+import logo from '../../assets/qargo.jpg';
 
 function Login() {
   const [email, setEmail] = useState('test@example.com');
@@ -40,6 +41,13 @@ function Login() {
   return (
     <div className="min-h-[calc(100vh-4rem)] flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
+        <div className="flex justify-center">
+          <img 
+            src={logo} 
+            alt="logo" 
+            className="w-50 h-50 object-contain"
+          />
+        </div>
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
